4d23a75f9c203bf9de60b80c5ad938e4511d2554
Bhavyam Mittal <bhavyamm01@gmail.com>
2021-11-29 13:41:57 +0530

    Landing page design completed

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+  let navLinks;
+  useEffect(() => {
+    navLinks = document.getElementById("navLinks");
+
+    console.log(navLinks, "nav links");
+  }, []);
+
+  const showMenu = () => {
+    navLinks.style.right = "0px";
+  };
+
+  const hideMenu = () => {
+    navLinks.style.right = "-200px";
+  };
+
+  return (
+    <div className="header">
+      <nav>
+        <Link to="/">
+          {/* <i className="fa fa-guitar" style={{ color: "#fff" }}></i> */}
+        </Link>
+        <div className="nav-links" id="navLinks">
+          <i className="fa fa-times" onClick={hideMenu}></i>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/courses">Courses</Link>
+            </li>
+            <li>
+              <Link to="/activities">Activities</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <i className="fa fa-bars" onClick={showMenu}></i>
+      </nav>
+
+      <div className="text-box">
+        <h1>World's Biggest University</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+          quas nostrum vitae
+          <br /> cumque incidunt laboriosam ipsam fuga similique, eum dolorem?
+        </p>
+        <Link to="/" className="hero-btn">
+          Visit us to know more
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
